0cc7d8962eb03948a9c8f8d129fd5c71c13a02ed
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-12-27 11:28:24 +0530

    form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Header from "./Header";
-import BG from "../assets/login-bgc.jpg";
+import BG from "../assets/bgc.jpg";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
   const [isSignedIn, setIsSignedIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
   const toggleSignInForm = () => {
     setIsSignedIn(!isSignedIn);
   };
 
+  const handleButtonClick = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+    if (!isSignedIn) {
+      const message = checkValidData(
+        email.current.value,
+        password.current.value,
+        name.current.value
+      );
+      console.log(name.current.value);
+      setErrorMessage(message);
+    } else {
+      const message = checkValidData(
+        email.current.value,
+        password.current.value
+      );
+      setErrorMessage(message);
+    }
+  };
+
   useEffect(() => {
+    // this function is used for changing background image based on responsiveness
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 767);
     };
@@ -24,40 +51,45 @@ const Login = () => {
   }, []);
   return (
     <div
-      className={`w-full min-h-screen bg-opacity-95 bg-cover ${
-        isMobile ? "bg-black" : "bg-black brightness-75"
-      }`}
+      className={`w-full min-h-screen bg-cover  ${isMobile ? "bg-black" : " "}`}
       style={isMobile ? {} : { backgroundImage: `url(${BG})` }}
     >
       <Header />
-      <div className="w-full md:max-w-[450px] pt-6 md:pt-0 px-[5%] mx-auto lg:px-0  ">
+      <div className="w-full md:max-w-[450px] pt-6 md:pt-0 px-[5%] mx-auto md:px-0 ">
         <form
-          action=""
-          className="flex text-white items-start flex-col  md:w-12/12 md:mx-auto  lg:w-12/12 bg-black md:bg-opacity-70 md:px-16 md:py-14 rounded-md "
+          onSubmit={(e) => e.preventDefault()}
+          className="flex text-white items-start flex-col  md:w-12/12 md:mx-auto  lg:w-12/12 bg-black md:bg-opacity-70 md:px-16 md:py-14 rounded-md  "
         >
           <h1 className="text-white text-3xl font-semibold mb-6 opacity-100">
             {isSignedIn ? "Sign In" : " Sign Up"}
           </h1>
           {!isSignedIn && (
             <input
+              ref={name}
               type="text"
               placeholder="Full Name"
               className="px-5 py-4 my-2 w-full rounded-md bg-[#333333]"
             />
           )}
           <input
+            ref={email}
             type="text"
             placeholder="Email or phone number"
             className="px-5 py-4 my-2 w-full rounded-md bg-[#333333]"
           />
           <input
-            type="password"
+            ref={password}
+            type="text"
             placeholder="Password"
             className="px-5 py-4 my-2 w-full rounded-md bg-[#333333]"
           />
-          <button className="px-5 my-6 py-3 text-white font-bold rounded-md bg-red-600 w-full">
+          <button
+            className="px-5 my-6 py-3 text-white font-bold rounded-md bg-red-600 w-full"
+            onClick={handleButtonClick}
+          >
             {isSignedIn ? "Sign In" : " Sign Up"}
           </button>
+          <p className="text-red-500 font-bold text-lg py-3">{errorMessage}</p>
           <p className="my-3 cursor-pointer" onClick={toggleSignInForm}>
             {isSignedIn
               ? "New to Netflix? Sign up now."
